d2bbeff46523518c3260cff0378db26ffd75e30a
Dan <pelusodan@gmail.com>
2023-09-05 13:13:28 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/spider_water/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.spider_water
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'dart:ui';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:spider_water/shows.dart';
+import 'dart:js' as js;
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class DraggableScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices =>
+      {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp
+
+  ({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scrollBehavior: DraggableScrollBehavior(),
+      title: 'Flutter Demo',
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Spider Water'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage
+
+  ({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    double screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              onUrlTapped("https://www.instagram.com/spiderwaterband/");
+            },
+            child: Tooltip(
+              message: "please god dont press me please man",
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset(
+                  'assets/img/face-pic.png',
+                  width: 300,
+                  height: 300,
+                ),
+              ),
+            )
+          ),
+          DefaultTextStyle(
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: screenHeight / 8,
+              fontFamily: 'Blockstepped',
+            ),
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText('spider water',
+                    speed: const Duration(milliseconds: 500)),
+              ],
+              isRepeatingAnimation: false,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(20)),
+          ShowsView(screenHeight),
+        ],
+      ),
+    );
+  }
+}
+
+void onUrlTapped(String? repoLink) {
+  if (repoLink == null) return;
+  js.context.callMethod('open', [repoLink]);
+}
+
+class ShowsView extends StatelessWidget {
+  final double screenHeight;
+
+  ShowsView(this.screenHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    var futureDates =
+    ShowList.where((element) => element.date.isAfter(DateTime.now()))
+        .toList();
+    final DateFormat formatter = DateFormat('M-dd');
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        for (var show in futureDates)
+          GestureDetector(
+            onTap: () {
+              onUrlTapped(show.url);
+            },
+            child: Row(
+              children: [
+                DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: screenHeight / 23,
+                    fontFamily: 'Blockstepped',
+                  ),
+                  child: Text(
+                    "-${show.title}",
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.all(5)),
+                DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: screenHeight / 30,
+                    fontFamily: 'Blockstepped',
+                  ),
+                  child: Text(
+                    "${formatter.format(show.date)} @ ${DateFormat.jm().format(
+                        show.date)}",
+                  ),
+                ),
+              ],
+            ),
+          )
+      ],
+    );
+  }
+}

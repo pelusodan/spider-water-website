75dfc0d2285af3c3906c1734249d9727cdbb13d0
Dan <pelusodan@gmail.com>
2023-09-05 13:49:27 -0400

    new site name and description

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
+import 'dart:js' as js;
 import 'dart:ui';
 
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:spider_water/shows.dart';
-import 'dart:js' as js;
-
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +13,7 @@ void main() {
 class DraggableScrollBehavior extends MaterialScrollBehavior {
   // Override behavior methods and getters like dragDevices
   @override
-  Set<PointerDeviceKind> get dragDevices =>
-      {
+  Set<PointerDeviceKind> get dragDevices => {
         PointerDeviceKind.touch,
         PointerDeviceKind.mouse,
         // etc.
@@ -24,16 +21,14 @@ class DraggableScrollBehavior extends MaterialScrollBehavior {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp
-
-  ({super.key});
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       scrollBehavior: DraggableScrollBehavior(),
-      title: 'Flutter Demo',
+      title: 'SPIDER WATER',
       theme: ThemeData(scaffoldBackgroundColor: Colors.black),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Spider Water'),
@@ -42,9 +37,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage
-
-  ({super.key, required this.title});
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -55,35 +48,28 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    double screenHeight = MediaQuery
-        .of(context)
-        .size
-        .height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
 
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           GestureDetector(
-            onTap: () {
-              onUrlTapped("https://www.instagram.com/spiderwaterband/");
-            },
-            child: Tooltip(
-              message: "please god dont press me please man",
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Image.asset(
-                  'assets/img/face-pic.png',
-                  width: 300,
-                  height: 300,
+              onTap: () {
+                onUrlTapped("https://www.instagram.com/spiderwaterband/");
+              },
+              child: Tooltip(
+                message: "please god dont press me please man",
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset(
+                    'assets/img/face-pic.png',
+                    width: 300,
+                    height: 300,
+                  ),
                 ),
-              ),
-            )
-          ),
+              )),
           DefaultTextStyle(
             style: TextStyle(
               color: Colors.black,
@@ -100,6 +86,17 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           const Padding(padding: EdgeInsets.all(20)),
           ShowsView(screenHeight),
+          const Padding(padding: EdgeInsets.all(40)),
+          DefaultTextStyle(
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: screenHeight / 40,
+              fontFamily: 'Blockstepped',
+            ),
+            child: const Text(
+              "cambridge ma synth pop for debutants",
+            ),
+          ),
         ],
       ),
     );
@@ -119,8 +116,8 @@ class ShowsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var futureDates =
-    ShowList.where((element) => element.date.isAfter(DateTime.now()))
-        .toList();
+        ShowList.where((element) => element.date.isAfter(DateTime.now()))
+            .toList();
     final DateFormat formatter = DateFormat('M-dd');
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -150,8 +147,7 @@ class ShowsView extends StatelessWidget {
                     fontFamily: 'Blockstepped',
                   ),
                   child: Text(
-                    "${formatter.format(show.date)} @ ${DateFormat.jm().format(
-                        show.date)}",
+                    "${formatter.format(show.date)} @ ${DateFormat.jm().format(show.date)}",
                   ),
                 ),
               ],
